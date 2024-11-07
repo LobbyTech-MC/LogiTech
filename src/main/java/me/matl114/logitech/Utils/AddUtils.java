95bdb4e4a612660f036fc0f9279468f9ab06c1da
LobbyTech <1187755698@qq.com>
2024-11-07 22:50:03 +0800

    适配1.21.1

--- src/main/java/me/matl114/logitech/Utils/AddUtils.java
+++ src/main/java/me/matl114/logitech/Utils/AddUtils.java
@@ -765,7 +765,7 @@ public class AddUtils {
     public static ItemStack addGlow(ItemStack stack){
         //stack.addEnchantment(Enchantment.ARROW_INFINITE, 1);
         ItemMeta meta=stack.getItemMeta();
-        meta.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+        meta.addEnchant(Enchantment.INFINITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         stack.setItemMeta(meta);
         return stack;
@@ -778,7 +778,7 @@ public class AddUtils {
         meta.addItemFlags(ItemFlag.HIDE_DESTROYS);
         meta.addItemFlags(ItemFlag.HIDE_DYE);
         meta.addItemFlags(ItemFlag.HIDE_PLACED_ON);
-        meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
+        meta.addItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
         stack.setItemMeta(meta);
         return stack;
     }
@@ -790,7 +790,7 @@ public class AddUtils {
         meta.removeItemFlags(ItemFlag.HIDE_DESTROYS);
         meta.removeItemFlags(ItemFlag.HIDE_DYE);
         meta.removeItemFlags(ItemFlag.HIDE_PLACED_ON);
-        meta.removeItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
+        meta.removeItemFlags(ItemFlag.HIDE_ADDITIONAL_TOOLTIP);
         stack.setItemMeta(meta);
         return stack;
     }
